93173b0b98e8e58642ea01fb50f4a1d43e88983a
Ayush <singhayush484h@gmail.com>
2023-04-01 21:21:47 +0530

    finalise query type

---
+++ src/graphql/Queries/index.ts
@@ -0,0 +1,8 @@
+import "./Book/getBook.js";
+import "./Book/getAllBooks.js";
+import "./Author/getAuthor.js";
+import "./Author/getAllAuthors.js";
+import "./Genre/getGenre.js";
+import "./Genre/getAllGenres.js";
+import builder from "../../utils/schemaBuilder.js";
+builder.queryType({});
